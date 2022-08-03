84f2326b53256a977a0407d02fd578e99460a44f
Jesse <jgodwin1420@gmail.com>
2022-08-03 15:41:21 +0100

    gastby image

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,15 @@ module.exports = {
   plugins: [
     "gatsby-plugin-mdx",
     "gatsby-plugin-postcss",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `./src/images`,
+      },
+    },
     {
       resolve: "gatsby-source-filesystem",
       options: {
@@ -13,13 +22,14 @@ module.exports = {
         path: "./src/pages/",
       },
       __key: "pages",
-    }, {
+    },
+    {
       resolve: "gatsby-plugin-react-svg",
       options: {
         rule: {
-          include: /images/ // See below to configure properly https://www.gatsbyjs.com/plugins/gatsby-plugin-react-svg/
-        }
-      }
+          include: /images/, // See below to configure properly https://www.gatsbyjs.com/plugins/gatsby-plugin-react-svg/
+        },
+      },
     },
   ],
 };
