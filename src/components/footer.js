094b515c92f9e46e5466831f5cdfd70205811f1b
jgodw <c1022253@gmail.com>
2022-12-24 09:21:34 +0000

    added a padding top

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import * as React from 'react';
 const Footer = () => {
     return (
         <footer>
-            <hr className="border-gray-200 sm:mx-auto dark:border-gray-700" />
+            <hr className="border-gray-200 sm:mx-auto dark:border-gray-700 pt-3" />
             <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
                 © {new Date().getFullYear()} Jesse Doka. All rights reserved.
             </span>
