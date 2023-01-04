300f74e3fb1eb75c955a95f8443ccb9d4c9ec5a6
Jesse Doka <jgodwin1420@gmail.com>
2023-01-04 16:20:33 +0000

    minmal image gallery

--- src/pages/photos/index.js
+++ src/pages/photos/index.js
@@ -1,31 +1,53 @@
-import * as React from "react"
+// useState
+import React, { useState } from "react"
 import Navbar from "../../components/navbar"
 import Footer from "../../components/footer"
 import { graphql } from "gatsby"
 import Img from "gatsby-image"
 
+
 // markup
 const PhotoPage = ({ data }) => {
     const { nodes } = data.allFile
     console.log(nodes)
+    const [i, setI] = useState(0)
+
+    function Increment() {
+        if (i < nodes.length - 1) {
+            setI(i + 1)
+        } else {
+            setI(0)
+        }
+    }
+
+    function Decrement() {
+        if (i > 0) {
+            setI(i - 1)
+        } else {
+            setI(nodes.length - 1)
+        }
+    }
+
     return (
         <main>
             <Navbar pageTitle={"Photos"} />
-                {/* click through each image */}
-            <div>
-                {nodes.map((node) => (
-                    <div className="mx-auto max-w-xl">
-                        <Img fluid={node.childImageSharp.fluid} />
-                    </div>
-                ))}
+            <div className="mx-auto sm:w-1/2 pt-9">
+                <div className="align-middle">
+                    <Img
+                        fadeIn={true}
+                        fluid={nodes[i].childImageSharp.fluid}
+
+                        className= {
+                            nodes[i].childImageSharp.fluid.sizes.split(' ').pop().split('px')[0] < 2048 ?  "sm:w-1/2 mx-auto" : "w-full mx-auto pt-9"
+                        }
+                    /> 
+                </div>
+            </div>
+            <div class="absolute top-0 bottom-12 w-full left-0 flex opacity-0">
+                <button class="w-1/2" onClick={Decrement}>Previous</button>
+                <button class="w-1/2" onClick={Increment}>Next</button>
             </div>
 
-                
-            
-            
-
-            
-           
             <Footer socialink={{
                 name: "@dokasgram",
                 url: "https://www.instagram.com/dokasgram/"
@@ -39,8 +61,8 @@ export const query = graphql`
         allFile(filter: {extension: {regex: "/(jpg)|(jpeg)|(png)/"}}) {
             nodes {
                 childImageSharp {
-                    fluid {
-                        ...GatsbyImageSharpFluid
+                    fluid (maxWidth: 2048, quality: 100){
+                        ...GatsbyImageSharpFluid_tracedSVG
                     }
                 }
             }
